33d7388e81b7667a5f951536f95304aedef43922
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-05-25 23:42:05 +0300

    RocketDetail page is done

--- src/components/Rockets/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Rockets = () => {
-  return (
-    <div>Rockets</div>
-  )
-}
-
-export default Rockets
--- src/pages/RocketDetail/index.js
+++ src/pages/RocketDetail/index.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+//components
+import Wave from "../../components/Shared/Wave";
+import Container from "../../components/Shared/Container";
+import RocketItem from "../../components/RocketItem";
+//providers
+import { LaunchContext } from "../../context";
+
 
 const RocketDetail = () => {
-  return (
-    <div>RocketDetail</div>
-  )
-}
+  const { rocketId } = useParams();
+  const { getRocket } = useContext(LaunchContext);
+  const rocket = getRocket(rocketId);
 
-export default RocketDetail
+  return (
+    <Container>
+      <Wave />
+      <RocketItem
+        name={rocket?.name}
+        description={rocket?.description}
+        country={rocket?.country}
+        firstFlight={rocket?.first_flight}
+        diameter={rocket?.diameter?.meters}
+        height={rocket?.height?.meters}
+        imageUrl={rocket?.flickr_images[1]}
+      />
+    </Container>
+  );
+};
+
+export default RocketDetail;
